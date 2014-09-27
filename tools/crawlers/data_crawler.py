9af33fd2b3d77567d84c14a92d527d58c1908ec9
Umayah Abdennabi <abden003@gmail.com>
2014-09-27 16:28:09 -0500

    Update datasets

--- tools/crawlers/data_crawler.py
+++ tools/crawlers/data_crawler.py
@@ -1,6 +1,6 @@
-import pull_target
-x = ["socks", "pants", "shirt", "shoes", "toy", "toys", "blanket", "pillow"]
+import pull_info
+x = ["shirt", "shoes", "toy", "toys", "blanket", "pillow"]
 for i in x:
-	pull_target.pull_info("walmart", i)
-	pull_target.pull_info("target", i)
+	pull_info.pull_info("walmart", i)
+	pull_info.pull_info("target", i)
 	
